278b6fae89141593cb430aa1661da2913084b465
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 19:10:56 -0800

    Implemented `survey` endpoints

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,10 +1,29 @@
 class SurveysController < ApplicationController
   def index
+    @surveys = Survey.order(:created_at)
   end
 
   def show
+    @survey = Survey.find(show_params[:id])
+    @responses = @survey.responses&.order(created_at: :desc)
   end
 
   def create
+    @survey = Survey.new(create_params)
+    if @survey.save
+      redirect_to surveys_path
+    else
+      redirect_to new_survey_path, alert: @survey.errors.full_messages.join(", ")
+    end
+  end
+
+  private
+
+  def show_params
+    params.permit(:id)
+  end
+
+  def create_params
+    params.permit(:question)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'surveys/index'
-  get 'surveys/show'
-  get 'surveys/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root 'surveys#index'
+  resources :surveys, only: %i[index show create]
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
