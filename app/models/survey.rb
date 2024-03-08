fc6ce6bba2387fe58209f5a2aaec97dccda4d032
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 21:07:27 -0800

    Cleaned/refactored logic

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,16 +3,19 @@ class Survey < ApplicationRecord
   has_many :responses, dependent: :destroy
 
   def yes_percentage
-    response_count = yes_responses_count + no_responses_count
-    return 0 unless response_count.positive?
-
-    (yes_responses_count.to_f / response_count * 100).round(2)
+    response_percentage(yes_responses_count)
   end
 
   def no_percentage
+    response_percentage(no_responses_count)
+  end
+
+  private
+  
+  def response_percentage(response_type)
     response_count = yes_responses_count + no_responses_count
     return 0 unless response_count.positive?
-
-    (no_responses_count.to_f / response_count * 100).round(2)
+  
+    (response_type.to_f / response_count * 100).round(2)
   end
 end
