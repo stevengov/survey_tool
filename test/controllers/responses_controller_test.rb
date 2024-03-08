cddefb8b6b34950c77d6e9f76a59f23be14eabd0
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 19:30:24 -0800

    Added controller tests

--- test/controllers/responses_controller_test.rb
+++ test/controllers/responses_controller_test.rb
@@ -1,13 +1,54 @@
 require "test_helper"
 
 class ResponsesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get responses_new_url
-    assert_response :success
+  setup do
+    @survey = surveys(:one)
+    @response_type = Response::RESPONSE_TYPE_MODELS.keys.first 
   end
 
-  test "should get create" do
-    get responses_create_url
+  test "#new - when survey_id is present - should get new" do
+    get new_survey_response_url(@survey.id)
     assert_response :success
+    assert_not_nil assigns(:survey)
+
+    assert_select 'h1', text: @survey.question
+    assert_select 'form' do
+      assert_select 'input[type=radio][id=response_type_yes]'
+      assert_select 'input[type=radio][id=response_type_no]'
+      assert_select 'input[type=submit]'
+    end
+    assert_select 'a.btn.btn-secondary.mt-3[href=?]', surveys_path, text: 'Back to Surveys'
+  end
+
+  test "#new - when survey_id is not present - should not get new" do
+    assert_raises(ActionController::UrlGenerationError) do
+      get new_survey_response_url(nil)
+      assert_nil assigns(:survey)
+    end
+  end
+
+  test "#create - when survey_id is not present - should not create response" do
+    assert_raises(ActionController::UrlGenerationError) do
+      post survey_responses_url(nil), params: { response: { response_type: @response_type, survey_id: nil } }
+      assert_nil assigns(:response)
+    end
+  end
+
+  test "#create - when response_type is not present - should not create response" do
+    assert_no_difference('Response.count') do
+      post survey_responses_url(@survey), params: { response: { response_type: nil, survey_id: @survey.id } }
+      assert_nil assigns(:response)
+    end
+
+    assert_redirected_to new_survey_response_url(@survey.id)
+  end
+
+  test "#create - when params are valid - should create response" do
+    assert_difference('Response.count') do
+      post survey_responses_url(@survey), params: { response: { response_type: @response_type, survey_id: @survey.id } }
+      assert_not_nil assigns(:response)
+    end
+
+    assert_redirected_to survey_url(Response.last.survey)
   end
 end
--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -1,18 +1,81 @@
 require "test_helper"
 
 class SurveysControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get surveys_index_url
-    assert_response :success
+  setup do
+    @survey = surveys(:one)
   end
 
-  test "should get show" do
-    get surveys_show_url
+  test "#index - should get index" do
+    get surveys_url
     assert_response :success
+    assert_not_nil assigns(:surveys)
+
+    assert_select 'th', text: 'Question'
+    assert_select 'th', text: 'Yes'
+    assert_select 'th', text: 'No'
+    assert_select 'th', text: 'Action'
+
+    assert_select 'td', text: @survey.question
+    assert_select 'td', text: @survey.yes_percentage.to_s + '%'
+    assert_select 'td', text: @survey.no_percentage.to_s + '%'
+
+    assert_select 'a.btn.btn-primary.mb-4[href=?]', new_survey_path, text: '+ Create New Survey'
+    assert_select 'a[href=?]', survey_path(@survey), text: 'Show'
+    assert_select 'a[href=?]', new_survey_response_path(@survey), text: 'New'
   end
 
-  test "should get create" do
-    get surveys_create_url
+  test "#show - when id is not present - should show survey" do
+    assert_raises(ActionController::UrlGenerationError) do
+      get survey_url(nil)
+      assert_response :success
+      assert_not_nil assigns(:survey)
+      assert_not_nil assigns(:responses)
+    end
+  end
+
+  test "#show - when id is present - should show survey" do
+    get survey_url(@survey)
     assert_response :success
+    assert_not_nil assigns(:survey)
+    assert_not_nil assigns(:responses)
+
+    assert_select 'h1', text: @survey.question
+    assert_select 'a.btn.btn-secondary.mt-3[href=?]', surveys_path, text: 'Back to Surveys'
+
+    assert_select 'p', text: 'Yes: ' + @survey.yes_percentage.to_s + '%, No: ' + @survey.no_percentage.to_s + '%'
+
+    assert_select 'th', text: 'Response'
+    assert_select 'th', text: 'Time'
+
+    response = @survey.responses.first
+    assert_select 'td', text: response.display_type
+    assert_select 'td', text: response.created_at.strftime("%b %d, %Y, %I:%M %p")
+
+  end
+
+  test "#create - when question is not present - should not create survey" do
+    assert_no_difference('Survey.count') do
+      post surveys_url, params: { question: '' }
+    end
+
+    assert_redirected_to new_survey_path
+    assert_not_empty flash[:alert]
+  end
+
+  test "#create - when params are valid - should create survey" do
+    assert_difference('Survey.count') do
+      post surveys_url, params: { question: 'New Survey Question' }
+    end
+
+    assert_redirected_to surveys_path
+  end
+
+  test "#new - should get new" do
+    get new_survey_url
+    assert_select 'form' do
+      assert_select 'input[type=text]'
+      assert_select 'input[type=submit]'
+    end
+    assert_select 'a.btn.btn-secondary.mt-3[href=?]', surveys_path, text: 'Back to Surveys'
   end
 end
