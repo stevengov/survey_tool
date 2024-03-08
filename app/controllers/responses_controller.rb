11ee20a12d7807823802d987a2002322db2ab064
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 19:24:32 -0800

    Implemented `response` endpoints

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,7 +1,36 @@
 class ResponsesController < ApplicationController
   def new
+    @survey = Survey.find(new_params[:survey_id])
   end
 
-  def create
+  def create # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    klass = response_klass(create_params[:response_type])
+    unless klass
+      return redirect_to new_survey_response_path(create_params[:survey_id]),
+                         alert: "Must select a response"
+    end
+
+    @response = klass.new(survey_id: create_params[:survey_id])
+    if @response.save
+      redirect_to survey_path(@response.survey)
+    else
+      redirect_to new_survey_response_path(@response.survey),
+                  alert: @response.errors.full_messages.join(", ")
+    end
+  end
+
+
+  private
+
+  def create_params
+    params.require(:response).permit(:response_type, :survey_id)
+  end
+
+  def new_params
+    params.permit(:survey_id)
+  end
+
+  def response_klass(type)
+    Response::RESPONSE_TYPE_MODELS[type]&.constantize
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'responses/new'
-  get 'responses/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root 'surveys#index'
-  resources :surveys, only: %i[index show create new]
+  root "surveys#index"
+  resources :surveys, only: %i[index show create new] do
+    resources :responses, only: %i[new create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
