98eb8c7b87c5db9dfe1990805255007c9e227e00
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 19:21:25 -0800

    Created `responses_controller` with `new` and `create

---
+++ test/controllers/responses_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ResponsesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get responses_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get responses_create_url
+    assert_response :success
+  end
+end
