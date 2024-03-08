869b9f94aa05941e2eca36553f1c45b24632e0b5
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 18:57:37 -0800

    Updated `survey` model test

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -15,6 +15,30 @@ class SurveyTest < ActiveSupport::TestCase
     assert_not @survey.valid?
   end
 
+  test "should have many responses" do
+    assert_equal @survey.responses.new.class, NoResponse
+  end
+
+  test "should destroy associated responses" do
+    @survey.save
+    YesResponse.create(survey: @survey)
+    assert_difference "Response.count", -1 do
+      @survey.destroy
+    end
+  end
+
+  test "yes_percentage calculation" do
+    YesResponse.create(survey: @survey)
+    NoResponse.create(survey: @survey)
+    assert_equal 50.0, @survey.yes_percentage
+  end
+
+  test "no_percentage calculation" do
+    YesResponse.create(survey: @survey)
+    NoResponse.create(survey: @survey)
+    assert_equal 50.0, @survey.no_percentage
+  end
+
   test "yes_percentage with no responses" do
     assert_equal 0, @survey.yes_percentage
   end
