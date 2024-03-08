b9ebc5011e9652614b6875f0dd8f76c2c08f3aa3
stevengov <44922197+stevengov@users.noreply.github.com>
2024-03-07 18:40:14 -0800

    Created `responses` table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_08_022256) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_023758) do
+  create_table "responses", force: :cascade do |t|
+    t.integer "survey_id", null: false
+    t.string "type", default: "NoResponse", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["survey_id"], name: "index_responses_on_survey_id"
+  end
+
   create_table "surveys", force: :cascade do |t|
     t.string "question", null: false
     t.integer "yes_responses_count", default: 0, null: false
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_022256) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "responses", "surveys"
 end
